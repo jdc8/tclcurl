5476e411717abb98631e5bdf5d8c27637be9f34a
Jos Decoster <jos.decoster@gmail.com>
2014-01-29 22:39:24 +0100

    initial checkin

---
+++ build.tcl
@@ -0,0 +1,492 @@
+#!/bin/sh
+# -*- tcl -*- \
+exec tclsh "$0" ${1+"$@"}
+package require Tcl 8.5
+set me [file normalize [info script]]
+set packages {
+    tclcurl
+}
+set tclpackages {
+}
+proc main {} {
+    global argv tcl_platform tag
+    set tag {}
+    if {![llength $argv]} {
+	if {$tcl_platform(platform) eq "windows"} {
+	    set argv gui
+	} else {
+	    set argv help
+	}
+    }
+    if {[catch {
+	eval _$argv
+    }]} usage
+    exit 0
+}
+proc usage {{status 1}} {
+    global errorInfo
+    if {[info exists errorInfo] && ($errorInfo ne {}) &&
+	![string match {invalid command name "_*"*} $errorInfo]
+    } {
+	puts stderr $::errorInfo
+	exit
+    }
+
+    global argv0
+    set prefix "Usage: "
+    foreach c [lsort -dict [info commands _*]] {
+	set c [string range $c 1 end]
+	if {[catch {
+	    H${c}
+	} res]} {
+	    puts stderr "$prefix$argv0 $c args...\n"
+	} else {
+	    puts stderr "$prefix$argv0 $c $res\n"
+	}
+	set prefix "       "
+    }
+    exit $status
+}
+proc tag {t} {
+    global tag
+    set tag $t
+    return
+}
+proc myexit {} {
+    tag ok
+    puts DONE
+    return
+}
+proc log {args} {
+    global tag
+    set newline 1
+    if {[lindex $args 0] eq "-nonewline"} {
+	set newline 0
+	set args [lrange $args 1 end]
+    }
+    if {[llength $args] == 2} {
+	lassign $args chan text
+	if {$chan ni {stdout stderr}} {
+	    ::_puts {*}[lrange [info level 0] 1 end]
+	    return
+	}
+    } else {
+	set text [lindex $args 0]
+	set chan stdout
+    }
+    # chan <=> tag, if not overriden
+    if {[string match {Files left*} $text]} {
+	set tag warn
+	set text \n$text
+    }
+    if {$tag eq {}} { set tag $chan }
+    #::_puts $tag/$text
+
+    .t insert end-1c $text $tag
+    set tag {}
+    if {$newline} { 
+	.t insert end-1c \n
+    }
+
+    update
+    return
+}
+proc +x {path} {
+    catch { file attributes $path -permissions u+x }
+    return
+}
+proc grep {file pattern} {
+    set lines [split [read [set chan [open $file r]]] \n]
+    close $chan
+    return [lsearch -all -inline -glob $lines $pattern]
+}
+proc version {file} {
+    #puts GREP\t[join [grep $file {*package provide *}] \nGREP\t]
+    set v [lindex [grep $file {*package provide *}] 0 3]
+    puts "Version:  $v"
+    return $v
+}
+proc Hhelp {} { return "\n\tPrint this help" }
+proc _help {} {
+    usage 0
+    return
+}
+proc Hrecipes {} { return "\n\tList all brew commands, without details." }
+proc _recipes {} {
+    set r {}
+    foreach c [info commands _*] {
+	lappend r [string range $c 1 end]
+    }
+    puts [lsort -dict $r]
+    return
+}
+proc Hdrop {} { return "?destination?\n\tUninstall all packages.\n\tdestination = path of package directory, default \[info library\]." }
+proc _drop {{ldir {}}} {
+    global packages tclpackages
+    if {[llength [info level 0]] < 2} {
+	set ldir [info library]
+	set idir [file dirname [file dirname $ldir]]/include
+    } else {
+	set idir [file dirname $ldir]/include
+    }
+
+    foreach p $packages {
+	set src     [file dirname $::me]/$p.tcl
+	set version [version $src]
+
+	file delete -force $ldir/$p$version
+	puts -nonewline "Removed package:     "
+	tag ok
+	puts $ldir/$p$version
+    }
+
+    foreach {p file} $tclpackages {
+	set src     [file dirname $::me]/$file
+	set version [version $src]
+	set pdir    [string map {:: _} $p]
+
+	file delete -force $ldir/$pdir$version
+	puts -nonewline "Removed package:     "
+	tag ok
+	puts $ldir/$pdir$version
+    }
+    return
+}
+# proc Hdoc {} { return "\n\t(Re)Generate the embedded documentation." }
+# proc _doc {} {
+#     cd [file dirname $::me]/doc
+
+#     puts "Removing old documentation..."
+#     file delete -force ../embedded/man
+#     file delete -force ../embedded/www
+
+#     puts "Generating man pages..."
+#     exec 2>@ stderr >@ stdout dtplite        -o ../embedded/man -ext n nroff .
+#     puts "Generating 1st html..."
+#     exec 2>@ stderr >@ stdout dtplite -merge -o ../embedded/www html .
+#     puts "Generating 2nd html, resolving cross-references..."
+#     exec 2>@ stderr >@ stdout dtplite -merge -o ../embedded/www html .
+
+#     return
+# }
+proc configure {args} {
+    set curl ""
+    set openssl ""
+    set static 0
+    set ldir [info library]
+    set idir [file dirname [file dirname $ldir]]/include
+    set config ""
+    set n 0
+    while {[llength $args]} {
+	set args [lassign $args k]
+	if {[string length $k]} {
+	    switch -exact -- $k {
+		-curl {
+		    set args [lassign $args curl]
+		}
+		-openssl {
+		    set args [lassign $args openssl]
+		}
+		-static { set static 1 }
+		-dynamic { set static 0 }
+		default {
+		    if {$n == 0} {
+			set ldir $k
+			set idir [file dirname $ldir]/include
+		    } elseif {$n == 1} {
+			set config $k
+		    }
+		    incr n
+		}
+	    }
+	}
+    }
+    set d [dict create curl $curl openssl $openssl static $static ldir $ldir idir $idir config $config]
+    return $d
+}
+proc Hinstall {} { return "?destination? ?config? ?-curl <path>? ?-openssl <path>? ?-static? ?-dynamic?\n\tInstall all packages.\n\tdestination = path of package directory, default \[info library\].\n\tconfig = Critcl target to be used\n\t-curl <path> = path to Curl\n\t-openssl <path> = path to OpenSSL\n\t-static = link statically, default is to link dynamically\n\t-dynamic = link dynamically" }
+proc _install {args} {
+    global packages
+    set d [eval configure $args]
+    dict with d {}
+    # Create directories, might not exist.
+    file mkdir $idir
+    file mkdir $ldir
+
+    foreach p $packages {
+	set src     [file dirname $::me]/$p.tcl
+	set version [version $src]
+
+	file delete -force [pwd]/BUILD.$p
+
+	set rcargs [list]
+	lappend rcargs -with-mode [expr {$static?"static":"dynamic"}]
+	if {$config ne {}} {
+	    lappend rcargs -target $config
+	}
+	lappend rcargs -cache [pwd]/BUILD.$p -I [pwd]
+	if {[string length $curl]} {
+	    lappend rcargs -L [file join $curl lib] -I [file join $curl include]
+	} else {
+	    lappend rcargs -L $ldir
+	}
+	if {[string length $openssl]} {
+	    lappend rcargs -L [file join $openssl lib] -I [file join $openssl include]
+	} else {
+	    lappend rcargs -L $ldir
+	}
+	lappend rcargs -libdir $ldir -includedir $idir -pkg $src
+	RunCritcl {*}$rcargs
+
+	if {![file exists $ldir/$p]} {
+	    set ::NOTE {warn {DONE, with FAILURES}}
+	    break
+	}
+
+	file delete -force $ldir/$p$version
+	file rename        $ldir/$p $ldir/$p$version
+
+	puts -nonewline "Installed package:     "
+	tag ok
+	puts $ldir/$p$version
+	puts ""
+    }
+
+    Xinstalltclpackages $ldir
+    return
+}
+proc Hdebug {} { return "?destination? ?config? ?-curl <path>? ?-openssl <path>? ?-static? ?-dynamic?\n\tInstall debug builds of all packages.\n\tdestination = path of package directory, default \[info library\].\n\tconfig = Critcl target to be used\n\t-curl <path> = path to Curl\n\t-openssl <path> = path to OpenSSL\n\t-static = link statically, default is to link dynamically\n\t-dynamic = link dynamically" }
+proc _debug {args} {
+    global packages
+    set d [eval configure $args]
+    dict with d {}
+
+    # Create directories, might not exist.
+    file mkdir $idir
+    file mkdir $ldir
+
+    foreach p $packages {
+	set src     [file dirname $::me]/$p.tcl
+	set version [version $src]
+
+	file delete -force [pwd]/BUILD.$p
+
+	set rcargs [list]
+	lappend rcargs -with-mode [expr {$static?"static":"dynamic"}]
+	if {$config ne {}} {
+	    lappend rcargs -target $config
+	}
+	lappend rcargs -keep -debug all -cache [pwd]/BUILD.$p -I [pwd]
+	if {[string length $curl]} {
+	    lappend rcargs -L [file join $curl lib] -I [file join $curl include]
+	} else {
+	    lappend rcargs -L $ldir
+	}
+	if {[string length $openssl]} {
+	    lappend rcargs -L [file join $openssl lib] -I [file join $openssl include]
+	} else {
+	    lappend rcargs -L $ldir
+	}
+	lappend rcargs -libdir $ldir -includedir $idir -pkg $src
+	RunCritcl {*}$rcargs
+
+	file delete -force $ldir/$p$version
+	file rename        $ldir/$p $ldir/$p$version
+
+	puts -nonewline "Installed package:     "
+	tag ok
+	puts $ldir/$p$version
+    }
+
+    Xinstalltclpackages $ldir
+    return
+}
+proc Hgui {} { return "\n\tInstall all packages.\n\tDone from a small GUI." }
+proc _gui {} {
+    global INSTALLPATH CURLPATH OPENSSLPATH BUILDSTATIC
+    package require Tk
+#    package require widget::scrolledwindow
+
+    wm protocol . WM_DELETE_WINDOW ::_exit
+
+    label  .li -text {Install Path: }
+    entry  .ei -textvariable ::INSTALLPATH
+    button .bi -command [list Browse ::INSTALLPATH] -text Browse
+    label  .lz -text {Curl Path: }
+    entry  .ez -textvariable ::CURLPATH
+    button .bz -command [list Browse ::CURLPATH] -text Browse
+    label  .lo -text {Openssl Path: }
+    entry  .eo -textvariable ::OPENSSLPATH
+    button .bo -command [list Browse ::OPENSSLPATH] -text Browse
+    checkbutton .bs -text "Link statically" -variable BUILDSTATIC -anchor w
+    button .i -command Install -text Install
+    button .d -command Debug -text Debug
+
+#    widget::scrolledwindow .st -borderwidth 1 -relief sunken
+    text   .t
+#    .st setwidget .t
+
+    .t tag configure stdout -font {Helvetica 8}
+    .t tag configure stderr -background red    -font {Helvetica 12}
+    .t tag configure ok     -background green  -font {Helvetica 8}
+    .t tag configure warn   -background yellow -font {Helvetica 12}
+
+    grid .li .ei .bi .i -sticky nesw
+    grid .lz .ez .bz .d -sticky nesw
+    grid .bs - - -stick ewns
+    grid .t - - - -sticky nesw
+
+    grid rowconfigure . 0 -weight 0
+    grid rowconfigure . 3 -weight 1
+
+    grid columnconfigure . 0 -weight 0
+    grid columnconfigure . 1 -weight 1
+    grid columnconfigure . 2 -weight 0
+    grid columnconfigure . 3 -weight 0
+
+    set INSTALLPATH [info library]
+    set CURLPATH ""
+    set OPENSSLPATH ""
+
+    # Redirect all output into our log window, and disable uncontrolled exit.
+    rename ::puts ::_puts
+    rename ::log ::puts
+    rename ::exit   ::_exit
+    rename ::myexit ::exit
+
+    # And start to interact with the user.
+    vwait forever
+    return
+}
+proc Browse {varnm} {
+    upvar $varnm var
+    set d [tk_chooseDirectory -initialdir $var -title "Choose [string trim $varnm :]"]
+    if {[string length $d]} {
+	set var $d
+    }
+}
+proc Install {} {
+    global INSTALLPATH CURLPATH OPENSSLPATH NOTE
+    foreach p {.ei .ez .bi .bz .bs .i .d} {
+	$p configure -state disabled
+    }
+
+    set NOTE {ok DONE}
+    set fail [catch [format {
+	_install $INSTALLPATH -curl $CURLPATH -openssl $OPENSSLPATH %s
+
+	puts ""
+	tag  [lindex $NOTE 0]
+	puts [lindex $NOTE 1]
+    } [expr {$::BUILDSTATIC?"-static":"-dynamic"}]] e o]
+
+    .i configure -state normal
+    .i configure -command ::_exit -text Exit -bg green
+
+    if {$fail} {
+	# rethrow
+	return {*}$o $e
+    }
+    return
+}
+proc Debug {} {
+    global INSTALLPATH CURLPATH OPENSSLPATH
+    foreach p {.ei .ez .bi .bz .bs .i .d} {
+	$p configure -state disabled
+    }
+
+    set fail [catch [format {
+	_debug $INSTALLPATH -curl $CURLPATH -openssl $OPENSSLPATH %s
+
+	puts ""
+	tag ok
+	puts DONE
+    } [expr {$::BUILDSTATIC?"-static":"-dynamic"}]] e o]
+
+    .d configure -state normal
+    .d configure -command ::_exit -text Exit -bg green
+
+    if {$fail} {
+	# rethrow
+	return {*}$o $e
+    }
+    return
+}
+
+proc Xinstalltclpackages {ldir} {
+    global tclpackages
+
+    foreach {p file} $tclpackages {
+	set src     [file dirname $::me]/$file
+	set version [version $src]
+	set pdir    [string map {:: _} $p]
+
+	file mkdir $ldir/$pdir
+	file copy $src $ldir/$pdir
+	Xindex $p $version $file $ldir/$pdir
+
+	file delete -force $ldir/$pdir$version
+	file rename        $ldir/$pdir $ldir/$pdir$version
+
+	puts -nonewline "Installed Tcl package:     "
+	tag ok
+	puts $ldir/$pdir$version
+    }
+    return
+}
+
+proc Xindex {name version pfile dstdir} {
+    set    c [open $dstdir/pkgIndex.tcl w]
+    puts  $c "package ifneeded $name $version \[list ::apply {{dir} {\n\tsource \$dir/$pfile\n}} \$dir\]"
+    close $c
+    return
+}
+
+proc RunCritcl {args} {
+    #puts [info level 0]
+    if {![catch {
+	package require critcl::app 3.0
+    }]} {
+	#puts "......... [package ifneeded critcl::app [package present critcl::app]]"
+	critcl::app::main $args
+	return
+    } else {
+	foreach cmd {
+	    critcl3 critcl3.kit critcl3.tcl critcl3.exe
+	    critcl critcl.kit critcl.tcl critcl.exe
+	} {
+	    # Locate the candidate.
+	    set cmd [auto_execok $cmd]
+	    # Ignore applications which were not found.
+	    if {![llength $cmd]} continue
+
+	    # Proper native path needed, especially on windows. On
+	    # windows this also works (best) with a starpack for
+	    # critcl, instead of a starkit.
+
+	    set cmd [file nativename [lindex [auto_execok $cmd] 0]]
+
+	    # Ignore applications which are too old to support
+	    # -v|--version, or are too old as per their returned
+	    # version.
+	    set v ?????
+	    if {[catch {
+		set v [eval [list exec $cmd --version]]
+	    }] || ([package vcompare $v 3.0] < 0)} {
+		puts "v=$v"
+		continue
+	    }
+
+	    # Perform the requested action.
+	    set cmd [list exec 2>@ stderr >@ stdout $cmd {*}$args]
+	    #puts "......... $cmd"
+	    eval $cmd
+	    return
+	}
+    }
+
+    puts "Unable to find a usable critcl 3 application (package). Stop."
+    ::exit 1
+}
+
+main
---
+++ tclcurl.tcl
@@ -0,0 +1,498 @@
+package require critcl 3
+
+namespace eval ::curl {
+}
+
+critcl::userconfig define mode {choose mode of Curl to build and link against.} {static dynamic}
+
+if {[string match "win32*" [::critcl::targetplatform]]} {
+    critcl::clibraries -llibcurl_a
+    switch -exact -- [critcl::userconfig query mode] {
+	static {
+	    critcl::cflags /DDLL_EXPORT
+	}
+	dynamic {
+	}
+    }
+} else {
+    switch -exact -- [critcl::userconfig query mode] {
+	static {
+	    critcl::clibraries -l:libcurl.a -l:libssl.a -l:libcrypto.a
+	}
+	dynamic {
+	    critcl::clibraries -lcurl -lssl -lcrypto
+	}
+    }
+
+    critcl::clibraries -lpthread -lm
+
+    if {[string match "macosx*" [::critcl::targetplatform]]} {
+	critcl::clibraries -lgcc_eh
+    } else {
+	critcl::clibraries -lrt -luuid
+    }
+}
+
+# Get local build configuration
+if {[file exists "[file dirname [info script]]/tclcurl_config.tcl"]} {
+    set fd [open "[file dirname [info script]]/tclcurl_config.tcl"]
+    eval [read $fd]
+    close $fd
+}
+
+critcl::tcl 8.5
+critcl::csources multi.c tclcurl.c
+critcl::tsources tclcurl_tcl.tcl
+
+
+critcl::ccode {
+
+#include "tclcurl.h"
+
+#include <sys/types.h>
+#ifdef _WIN32
+#else
+#include <unistd.h>
+#endif
+
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlInitObjCmd --
+#*
+#*  This procedure is invoked to process the "curl::init" Tcl command.
+#*  See the user documentation for details on what it does.
+#*
+#* Results:
+#*  A standard Tcl result.
+#*
+#* Side effects:
+#*  See the user documentation.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::init {cd interp objc objv} {
+
+    Tcl_Obj             *resultPtr;
+    CURL                *curlHandle;
+    struct curlObjData  *curlData;
+    char                *handleName;
+
+    curlData=(struct curlObjData *)Tcl_Alloc(sizeof(struct curlObjData));
+    if (curlData==NULL) {
+        resultPtr=Tcl_NewStringObj("Couldn't allocate memory",-1);
+        Tcl_SetObjResult(interp,resultPtr);
+        return TCL_ERROR;
+    }
+
+    memset(curlData, 0, sizeof(struct curlObjData));
+    curlData->interp=interp;
+
+    curlHandle=curl_easy_init();
+    if (curlHandle==NULL) {
+        resultPtr=Tcl_NewStringObj("Couldn't open curl handle",-1);
+        Tcl_SetObjResult(interp,resultPtr);
+        return TCL_ERROR;
+    }
+
+    handleName=curlCreateObjCmd(interp,curlData);
+
+    curlData->curl=curlHandle;
+
+    resultPtr=Tcl_NewStringObj(handleName,-1);
+    Tcl_SetObjResult(interp,resultPtr);
+    Tcl_Free(handleName);
+
+    return TCL_OK;
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlVersion --
+#*
+#*	This procedure is invoked to process the "curl::init" Tcl command.
+#*	See the user documentation for details on what it does.
+#*
+#* Parameters:
+#*  The stantard parameters for Tcl commands
+#*
+#* Results:
+#*	A standard Tcl result.
+#*
+#* Side effects:
+#*	See the user documentation.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::version {cd interp objc objv} {
+
+    Tcl_Obj     *versionPtr;
+    char        tclversion[200];
+
+    sprintf(tclversion,"TclCurl Version %s (%s)",TclCurlVersion,
+                                                 curl_version());
+    versionPtr=Tcl_NewStringObj(tclversion,-1);
+    Tcl_SetObjResult(interp,versionPtr);
+
+    return TCL_OK;
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlEscape --
+#*
+#*  This function is invoked to process the "curl::escape" Tcl command.
+#*  See the user documentation for details on what it does.
+#*
+#*
+#* Parameters:
+#*  The stantard parameters for Tcl commands
+#*
+#* Results:
+#*  A standard Tcl result.
+#*
+#* Side effects:
+#*  See the user documentation.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::escape {cd interp objc objv} {
+
+    Tcl_Obj        *resultObj;
+    char           *escapedStr;
+
+    escapedStr=curl_easy_escape(NULL,Tcl_GetString(objv[1]),0);
+
+    if(!escapedStr) {
+        resultObj=Tcl_NewStringObj("curl::escape bad parameter",-1);
+        Tcl_SetObjResult(interp,resultObj);
+        return TCL_ERROR;
+    }
+    resultObj=Tcl_NewStringObj(escapedStr,-1);
+    Tcl_SetObjResult(interp,resultObj);
+    curl_free(escapedStr);
+
+    return TCL_OK;
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlUnescape --
+#*
+#*  This function is invoked to process the "curl::Unescape" Tcl command.
+#*  See the user documentation for details on what it does.
+#*
+#*
+#* Parameters:
+#*  The stantard parameters for Tcl commands
+#*
+#* Results:
+#*  A standard Tcl result.
+#*
+#* Side effects:
+#*  See the user documentation.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::unescape {cd interp objc objv} {
+
+    Tcl_Obj        *resultObj;
+    char           *unescapedStr;
+
+    unescapedStr=curl_easy_unescape(NULL,Tcl_GetString(objv[1]),0,NULL);
+    if(!unescapedStr) {
+        resultObj=Tcl_NewStringObj("curl::unescape bad parameter",-1);
+        Tcl_SetObjResult(interp,resultObj);
+        return TCL_ERROR;
+    }
+    resultObj=Tcl_NewStringObj(unescapedStr,-1);
+    Tcl_SetObjResult(interp,resultObj);
+    curl_free(unescapedStr);
+
+    return TCL_OK;
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlVersionInfo --
+#*
+#*  This function invokes 'curl_version_info' to query how 'libcurl' was
+#*  compiled.
+#*
+#* Parameters:
+#*  The standard parameters for Tcl commands, but nothing is used.
+#*
+#* Results:
+#*  A standard Tcl result.
+#*
+#* Side effects:
+#*  See the user documentation.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::versioninfo {cd interp objc objv} {
+
+    int                            tableIndex;
+    int                            i;
+    curl_version_info_data        *infoPtr;
+    Tcl_Obj                       *resultObjPtr=NULL;
+    char                           tmp[7];
+
+    if (objc!=2) {
+        resultObjPtr=Tcl_NewStringObj("usage: curl::versioninfo -option",-1);
+        Tcl_SetObjResult(interp,resultObjPtr); 
+        return TCL_ERROR;
+    }
+
+    if (Tcl_GetIndexFromObj(interp, objv[1], versionInfoTable, "option",
+            TCL_EXACT,&tableIndex)==TCL_ERROR) {
+        return TCL_ERROR;
+    }
+
+    infoPtr=curl_version_info(CURLVERSION_NOW);
+
+    switch(tableIndex) {
+        case 0:
+            resultObjPtr=Tcl_NewStringObj(infoPtr->version,-1);
+            break;
+        case 1:
+            sprintf(tmp,"%X",infoPtr->version_num);
+            resultObjPtr=Tcl_NewStringObj(tmp,-1);
+            break;
+        case 2:
+            resultObjPtr=Tcl_NewStringObj(infoPtr->host,-1);
+            break;
+        case 3:
+            resultObjPtr=Tcl_NewListObj(0,(Tcl_Obj **)NULL);
+            if (infoPtr->features&CURL_VERSION_IPV6) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("IPV6",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_KERBEROS4) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("KERBEROS4",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_SSL) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("SSL",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_LIBZ) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("LIBZ",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_NTLM) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("NTLM",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_GSSNEGOTIATE) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("GSSNEGOTIATE",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_DEBUG) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("DEBUG",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_ASYNCHDNS) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("ASYNCHDNS",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_SPNEGO) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("SPNEGO",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_LARGEFILE) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("LARGEFILE",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_IDN) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("IDN",-1));
+            }
+            if (infoPtr->features&CURL_VERSION_SSPI) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("SSPI",-1));
+            }
+            break;
+            if (infoPtr->features&CURL_VERSION_CONV) {
+                Tcl_ListObjAppendElement(interp,resultObjPtr
+                        ,Tcl_NewStringObj("CONV",-1));
+            }
+        case 4:
+            resultObjPtr=Tcl_NewStringObj(infoPtr->ssl_version,-1);
+            break;
+        case 5:
+            resultObjPtr=Tcl_NewLongObj(infoPtr->ssl_version_num);
+            break;
+        case 6:
+            resultObjPtr=Tcl_NewStringObj(infoPtr->libz_version,-1);
+            break;
+        case 7:
+            resultObjPtr=Tcl_NewListObj(0,(Tcl_Obj **)NULL);
+            for(i=0;;i++) {
+                if (infoPtr->protocols[i]!=NULL) {
+                    Tcl_ListObjAppendElement(interp,resultObjPtr
+                            ,Tcl_NewStringObj(infoPtr->protocols[i],-1));
+                } else {
+                    break;
+                }
+            }
+    }
+
+    Tcl_SetObjResult(interp,resultObjPtr);
+
+    return TCL_OK;
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlShareInitObjCmd --
+#*
+#*  This procedure is invoked to process the "curl::shareinit" Tcl command.
+#*  See the user documentation for details on what it does.
+#*
+#* Results:
+#*  A standard Tcl result.
+#*
+#* Side effects:
+#*  See the user documentation.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::shareinit {cd interp objc objv} {
+
+    Tcl_Obj               *resultPtr;
+    CURL                  *shcurlHandle;
+    struct shcurlObjData  *shcurlData;
+    char                  *shandleName;
+
+    shcurlData=(struct shcurlObjData *)Tcl_Alloc(sizeof(struct shcurlObjData));
+    if (shcurlData==NULL) {
+        resultPtr=Tcl_NewStringObj("Couldn't allocate memory",-1);
+        Tcl_SetObjResult(interp,resultPtr);
+        return TCL_ERROR;
+    }
+
+    memset(shcurlData, 0, sizeof(struct shcurlObjData));
+
+    shcurlHandle=curl_share_init();
+    if (shcurlHandle==NULL) {
+        resultPtr=Tcl_NewStringObj("Couldn't create share handle",-1);
+        Tcl_SetObjResult(interp,resultPtr);
+        return TCL_ERROR;
+    }
+
+    shandleName=curlCreateShareObjCmd(interp,shcurlData);
+
+    shcurlData->shandle=shcurlHandle;
+
+    resultPtr=Tcl_NewStringObj(shandleName,-1);
+    Tcl_SetObjResult(interp,resultPtr);
+    Tcl_Free(shandleName);
+
+#ifdef TCL_THREADS
+    curl_share_setopt(shcurlHandle, CURLSHOPT_LOCKFUNC, curlShareLockFunc);
+    curl_share_setopt(shcurlHandle, CURLSHOPT_LOCKFUNC, curlShareUnLockFunc);
+#endif
+
+    return TCL_OK;
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlEasyStringError --
+#*
+#*  This function is invoked to process the "curl::easystrerror" Tcl command.
+#*  It will return a string with an explanation of the error code given.
+#*
+#* Results:
+#*  A standard Tcl result.
+#*
+#* Side effects:
+#*  The interpreter will contain as a result the string with the error
+#*  message.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::easystrerror {cd interp objc objv} {
+
+
+    if (objc<2) {
+        Tcl_WrongNumArgs(interp,1,objv,"errorCode");
+        return TCL_ERROR;
+    }
+
+    if (curlErrorStrings(interp,objv[1],0)) {
+        return TCL_ERROR;
+    }
+    return TCL_OK;
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlShareStringError --
+#*
+#*  This function is invoked to process the "curl::sharestrerror" Tcl command.
+#*  It will return a string with an explanation of the error code given.
+#*
+#* Results:
+#*  A standard Tcl result.
+#*
+#* Side effects:
+#*  The interpreter will contain as a result the string with the error
+#*  message.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::sharestrerror {cd interp objc objv} {
+
+    if (objc<2) {
+        Tcl_WrongNumArgs(interp,1,objv,"errorCode");
+        return TCL_ERROR;
+    }
+
+    if (curlErrorStrings(interp,objv[1],1)) {
+        return TCL_ERROR;
+    }
+    return TCL_OK;
+}
+
+#*
+#*----------------------------------------------------------------------
+#*
+#* curlMultiStringError --
+#*
+#*  This function is invoked to process the "curl::multirerror" Tcl command.
+#*  It will return a string with an explanation of the error code given.
+#*
+#* Results:
+#*  A standard Tcl result.
+#*
+#* Side effects:
+#*  The interpreter will contain as a result the string with the error
+#*  message.
+#*
+#*----------------------------------------------------------------------
+#*/
+critcl::ccommand ::curl::multistrerror {cd interp objc objv} {
+
+    if (objc<2) {
+        Tcl_WrongNumArgs(interp,1,objv,"errorCode");
+        return TCL_ERROR;
+    }
+
+    if (curlErrorStrings(interp,objv[1],2)) {
+        return TCL_ERROR;
+    }
+    return TCL_OK;
+}
+
+package provide TclCurl 7.22.0
