74494a23deda3408771730d4d44bb83f417929f4
Jos Decoster <jos.decoster@gmail.com>
2014-02-21 22:48:29 +0100

    add ftp tests

---
+++ tcltest_ftp_server/gen_ftp_files.tcl
@@ -0,0 +1,6 @@
+set f [open file1.txt w]
+puts $f "File 1"
+close $f
+set f [open file2.txt w]
+puts $f "File 2"
+close $f
--- tests/ftpList.tcl
+++
@@ -1,9 +0,0 @@
-package require TclCurl
-
-curl::transfer -url "ftp://127.0.0.1/pub/" -ftplistonly 1
-
-
-
-
-
-
